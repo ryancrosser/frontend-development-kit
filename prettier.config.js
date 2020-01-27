6d6a5d92cb2fa835424a2b6625d95d53283b223d
Ryan Crosser <ryankcrosser@gmail.com>
2020-01-26 21:52:38 -0500

    feature: Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none', // none|es5|all
+  bracketSpacing: false,
+  arrowParens: 'avoid' //, avoid|always
+};
